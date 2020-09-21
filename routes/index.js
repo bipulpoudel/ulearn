3e0fd6f7a944b96401733fc8018e3e1c3321e68b
unknown <bipulpoudel55555@gmail.com>
2020-09-21 23:08:07 +0545

    new update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+
+//import controllers
+const { get_landing } = require('../app/controllers/landing');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/',get_landing);
 
 module.exports = router;
